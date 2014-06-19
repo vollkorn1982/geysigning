57a1e65db6895f39284c13bed5b2da440359a905
Tobias Mueller <muelli@cryptobitch.de>
2014-06-19 13:58:44 +0200

    AvahiBrowser: Made it emit a gsignal on a new service of desired type
    DBus is.. weird. At least these (old fashioned?) bindings are.  It
    wouldn't take a normal mainloop.

--- AvahiBrowse.py
+++ AvahiBrowse.py
@@ -21,15 +21,21 @@ def print_error(*args):
     print args[0]
 
 
-class AvahiBrowser:
-    __gsignals__ = (
-        (),
-    )
+class AvahiBrowser(GObject.GObject):
+    __gsignals__ = {
+        'new_service': (GObject.SIGNAL_RUN_LAST, None,
+            # name, address (could be an int, too (for IPv4)), port
+            (str, str, int))
+    }
     
     def __init__(self, loop=None, service='_demo._tcp'):
+        GObject.GObject.__init__(self)
+        
         self.service = service
 
         self.loop = loop or DBusGMainLoop()
+        # It seems that these are different loops..?!
+        self.loop = DBusGMainLoop()
         self.bus = dbus.SystemBus(mainloop=self.loop)
         
         self.server = dbus.Interface( self.bus.get_object(avahi.DBUS_NAME, '/'),
@@ -53,14 +59,25 @@ class AvahiBrowser:
 
         self.server.ResolveService(interface, protocol, name, stype,
             domain, avahi.PROTO_UNSPEC, dbus.UInt32(0),
-            reply_handler=service_resolved, error_handler=print_error)
+            reply_handler=self.on_service_resolved,
+            error_handler=self.on_error)
+
+
+    def do_new_service(self, name, address, port):
+        print "Creating new signal"
 
     def on_service_resolved(self, *args):
         '''called when the browser successfully found a service'''
+        name = args[2]
+        address = args[7]
+        port = args[8]
         print 'service resolved'
-        print 'name:', args[2]
-        print 'address:', args[7]
-        print 'port:', args[8]
+        print 'name:', name
+        print 'address:', address
+        print 'port:', port
+        retval = self.emit('new_service', name, address, port)
+        print "emitted", retval
+
 
 
     def on_error(self, *args):
@@ -69,6 +86,15 @@ class AvahiBrowser:
 
 
 if __name__ == '__main__':
+    loop = GObject.MainLoop()
+    # We're not passing the loop to DBus, because... well, it
+    # does't work... It seems to expect a DBusMainLoop, not
+    # an ordinary main loop...
     ab = AvahiBrowser()
-    GObject.MainLoop().run()
+    
+    def print_signal(*args):
+        print "Signal ahoi", args
+    
+    ab.connect('new_service', print_signal)
+    loop.run()
     
