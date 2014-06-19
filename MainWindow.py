7f3bc49d077d89decfd01426f1b91f9cabb10786
Tobias Mueller <muelli@cryptobitch.de>
2014-06-19 15:09:19 +0200

    MainWindow: Set up the AvahiBrowser
    This is probably a bit weird.  I made it go in this place, because I
    think the discovery part should run as soon as possible, i.e. at the
    start of the application.  This is debatable, of course.  I'm also
    lacking a vision of how to get the data in to the signing page.

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,7 @@
+from gi.repository import GLib
 from gi.repository import Gtk
+
+from AvahiBrowse import AvahiBrowser
 from Sections import KeySignSection, GetKeySection
 
 class MainWindow(Gtk.Window):
@@ -15,3 +18,35 @@ class MainWindow(Gtk.Window):
 
         # setup signals
         self.connect("delete-event", Gtk.main_quit)
+        
+        self.avahi_browser = None
+        self.avahi_service_name = '_demo._tcp'
+        self.discovered_services = []
+        GLib.idle_add(self.setup_avahi_browser)
+
+
+    def setup_avahi_browser(self):
+        # FIXME: place a proper service string there
+        self.avahi_browser = AvahiBrowser(service=self.avahi_service_name)
+        self.avahi_browser.connect('new_service', self.on_new_service)
+        
+        return False
+
+
+    def on_new_service(self, browser, name, address, port):
+        if self.verify_service(name, address, port):
+            GLib.idle_add(self.add_discovered_service, name, address, port)
+
+
+    def verify_service(self, name, address, port):
+        '''A tiny function to return whether the service
+        is indeed something we are interested in'''
+        if name == self.avahi_service_name:
+            return True
+        else:
+            return False
+
+    def add_discovered_service(self, name, address, port):
+        self.discovered_services += (name, address, port)
+
+        return False
